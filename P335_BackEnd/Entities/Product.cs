88f69b8ff57bd1cd4381d8c4fdd0fdfcf6c8d561
Sənan Orucov <sorucov776@gmail.com>
2024-01-07 16:19:47 +0400

    Add project files.

---
+++ P335_BackEnd/Entities/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace P335_BackEnd.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Column(TypeName = "money")]
+        public decimal Price { get; set; }
+
+        [Required]
+        public string ImageId { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+
+        public List<ProductTypeProduct> ProductTypeProducts { get; set; }
+        public List<ProductImage> ProductImages { get; set; }
+    }
+}
+
---
+++ P335_BackEnd/Entities/ProductImage.cs
@@ -0,0 +1,11 @@
+﻿namespace P335_BackEnd.Entities
+{
+    public class ProductImage
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public string ImageUrl { get; set; }
+        public int Order { get; set; }
+        public Product Product { get; set; }
+    }
+}
